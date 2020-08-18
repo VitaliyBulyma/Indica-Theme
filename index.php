ea08ef7766f9daffeb73c70bf3372473ca63e1d0
VitaliyBulyma <55520185+VitaliyBulyma@users.noreply.github.com>
2020-08-18 11:50:51 -0400

    browser css and js caching disabled

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php get_header(); ?>
+<div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>);"></div>
+<img style="width:300px" src="<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>" />
 <?php
+
 while (have_posts()){
     the_post();?>
     <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
